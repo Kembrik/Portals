809023e1ce4e86d1d0a27b9a144d70dd5249fea1
AL_BOT <albot84@mail.ru>
2020-09-15 11:43:09 +0300

    Добавлена генерация подземелья до 12 комнат двух видов.

---
+++ Assets/Scripts/Dungeon_Procedural_Generation/PlayerTopDown.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerTopDown : MonoBehaviour
+{
+    private Rigidbody componentRigidbody;
+    private Vector3 moveDirection = Vector3.zero;
+
+    public int TurnSpeed = 2;
+
+    private void Start()
+    {
+        componentRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        componentRigidbody.AddForce(moveDirection * TurnSpeed);
+    }
+}
---
+++ Assets/Scripts/Dungeon_Procedural_Generation/Room.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public GameObject DoorU;
+    public GameObject DoorR;
+    public GameObject DoorD;
+    public GameObject DoorL;
+
+    public Material[] Meshes;
+
+    private void Start()
+    {
+        foreach (var comp in GetComponentsInChildren<Component>())
+        {
+            if (comp.name == "floor")
+            {
+                var material = comp.GetComponentsInChildren<Renderer>();
+                material[0].material = Meshes[Random.Range(0, Meshes.Length)];
+            }
+        }
+    }
+
+    public void RotateRandomly()
+    {
+        int count = Random.Range(0, 4);
+
+        for (int i = 0; i < count; i++)
+        {
+            transform.Rotate(0, 90, 0);
+
+            GameObject tmp = DoorL;
+            DoorL = DoorD;
+            DoorD = DoorR;
+            DoorR = DoorU;
+            DoorU = tmp;
+        }
+    }
+}
